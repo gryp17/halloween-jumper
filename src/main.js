0eff9cef908389c0d9301f27d156b796c05bfc26
Plamen Ivanov <plamen.ivanov@acronis.com>
2021-12-12 22:05:24 +0200

    Added the font-awesome package together with the necessary svg icons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faVolumeUp, faMusic } from '@fortawesome/free-solid-svg-icons';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+//add more icons if necessary
+library.add(faVolumeUp, faMusic);
+
+//enables the use of the <i> tag for the font awesome icons (<i class="fab fa-apple"></i>)
+dom.watch();
+
+/* how to use the font-awesome vue component:
+	<font-awesome-icon :icon="['fas', 'coffee']" />
+	<font-awesome-icon :icon="['far', 'angry']" />
+	<font-awesome-icon :icon="['fab', 'apple']" />
+*/
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
